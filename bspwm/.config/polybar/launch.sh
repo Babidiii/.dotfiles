888186ac4b0cd31e2b96d2fb1bda46e989958156
Babidiii <bilal.guillaumie@etu.uca.fr>
2021-09-01 22:59:50 +0200

    feat: polybar updated, bspwm updated for multi monitor

---
+++ bspwm/.config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar babidiiiBar 2>&1 | tee -a /tmp/polybar.log & disown
+#   done
+# else
+polybar babidiiiBar 2>&1 | tee -a /tmp/polybar.log & disown
+polybar extBabidiiiBar 2>&1 | tee -a /tmp/polybar-ext.log & disown
+# fi
+
+echo "Polybar launched..."
